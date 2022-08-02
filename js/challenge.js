0fcc2646cbd706c16f552cf27ae8c7c78b1e519b
Parker Parham <parkerparham@gmail.com>
2022-08-02 16:48:10 -0400

    Done with assignment

--- js/challenge.js
+++ js/challenge.js
@@ -33,13 +33,24 @@ let isPaused = false
     function likeNumber(){
         const heart = document.getElementById("heart");
         const likesList = document.querySelector(".likes");
+        let likes = 0;
+        let likeArray = [];
         // const displayLikes = document.createElement('li'); //putting this up here creates one li element with that const name. the event listener would just change the text content of it each time its invoked if i kept this up here 
         // console.log(likesList, displayLikes)
         heart.addEventListener("click", () => {
-            const displayLikes = document.createElement('li'); //by moving this const inside of the event listener, a new li is created each time the heart is clicked
-            displayLikes.innerText = `${seconds} was liked 8 times.`;
-            likesList.appendChild(displayLikes);
-            // likesList.innerText = `${seconds} was liked 8 times.`
+            if(seconds === likeArray.find(element => element === seconds)){
+                let likeNode = document.getElementById(`${seconds}`)
+                likes += 1
+                likeNode.innerText = `${seconds} was liked ${likes} times.`
+            }
+            else{ 
+                likes = 1
+                const displayLikes = document.createElement('li'); //by moving this const inside of the event listener, a new li is created each time the heart is clicked
+                displayLikes.id = seconds
+                displayLikes.innerText = `${seconds} was liked ${likes} time.`;
+                likesList.appendChild(displayLikes);
+                likeArray.push(seconds)
+            }
         })
     }
     likeNumber()
@@ -61,7 +72,7 @@ let isPaused = false
                 document.getElementById("minus").disabled = false
                 document.getElementById("plus").disabled = false
                 document.getElementById("heart").disabled = false
-                console.log("I was clicked while isPaused is true")
+                // console.log("I was clicked while isPaused is true")
                 // setInterval(() => seconds++, 1000)
                 firstInterval = setInterval(incrementSeconds, 1000) //if you put a let here, it redeclares that every time this is triggered which caused the increasing speed of the counter
             }
@@ -69,6 +80,21 @@ let isPaused = false
         })
     }
     pauseCounter()
+
+    function commentBox(){
+        const list = document.getElementById("list")
+        const input = document.getElementById("comment_input")
+        const form = document.getElementById("comment-form")
+        form.addEventListener('submit', (e) =>{
+            e.preventDefault()
+            let inputValue = e.target.comment_input.value
+            let p = document.createElement('p')
+            p.textContent = inputValue
+            list.appendChild(p)
+            form.reset()
+        })
+    }
+    commentBox()
 }
 
  editCounter()
